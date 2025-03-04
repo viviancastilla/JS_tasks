18176cdeba4305cf51c56e12a075c7fde0a15710
viviancastilla <vivi.castilla.gzz@hotmail.com>
2025-03-04 15:00:28 +0200

    more arrays

--- 05_week/array/array_01.js
+++ 05_week/array/array_01.js
@@ -17,9 +17,33 @@ let exclude = teamMembers.slice(2);
 console.log(exclude);
 
 //Task 7, 8
-let lookFor = teamMembers.find(teamMembers => teamMembers.Mike);
-let lookFor2 = teamMembers.find(teamMembers => teamMembers.Jake);
+let lookFor = teamMembers.find(member => member.Mike);
+let lookFor2 = teamMembers.find(member => member.Jake);
 
 //Task 9
-let replace = teamMembers.splice(0, 0, "Carol");
-console.log(replace);
+let replace = teamMembers.splice(2, 1, "Carol", "Bruce");
+console.log(teamMembers);
+
+//Task 10
+let addBob = [teamMembers, "Bob"];
+console.log(addBob);
+
+//Task 11
+let copy = teamMembers.slice();
+console.log(copy);
+
+//Task 12
+let newMembers = ['Tina', 'Dean'];
+let combined = teamMembers.concat(newMembers);
+console.log(combined);
+
+//Task 13
+let findJohn = [];
+for(let i = 0; 1 < teamMembers.length; i++){
+    if(teamMembers[i] === 'John'){
+        findJohn.push(i);
+    }
+}
+console.log(findJohn);
+
+//Task 14
