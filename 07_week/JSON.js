66e19d86686a78af56a8092adec80fdd82f8bd6c
viviancastilla <vivi.castilla.gzz@hotmail.com>
2025-03-04 22:18:42 +0200

    JSON

--- 07_week/JSON.js
+++ 07_week/JSON.js
@@ -22,7 +22,9 @@ console.log(classroomJSON);
 
 //Task 4
 let classroomObject = JSON.parse(classroomJSON);
-console.log(classroomObject);
+classroomObject.forEach(student => {
+    console.log(classroomObject);
+});
 
 //Task 5
 let weatherReport = {
@@ -43,38 +45,54 @@ let movies = [
 let moviesJSON = JSON.stringify(movies);
 let moviesObject = JSON.parse(moviesJSON);
 movies.forEach(movies => {
-    if(movies > 8){
-        console.log(movies.rating); 
+    if(movies.rating > 8){
+        console.log(movies.title); 
     }
 });
 
 //Task 7
-let filterHighRatedMovies(jsonString){
-let movieArray = JSON.parse(jsonString);
-let highRatedMovies = movieArray.filter(function(movie){
-        return ovie.rating > 8;
-   });
+function filterHighRatedMovies(jsonString){
+    let movieArray = JSON.parse(jsonString);
+    return movieArray.filter(function(movie){
+        return movie.rating > 8;
+    });
 }
+let highRatedMovies = filterHighRatedMovies(moviesJSON);
+console.log(highRatedMovies)
 
 //Task 8
-let shoppingCart = [
-    {items: }
-]
+let shoppingCart = {
+    items: [
+        {name: "cereal", price: 3},
+        {name: "yogurt", price: 1.19},
+        {name: "butter", price: 2.30}
+    ]
+}
+let shoppingCartJSON = JSON.stringify(shoppingCart);
+console.log(shoppingCartJSON);
 
 //Task 9
 function totalCartValue(jsonString){
-    
+    let object = JSON.parse(jsonString);
+    let total = 0;
+
+    object.items.forEach(item =>{
+        total = item.price + total;
+    });
+    return total;
 }
+let realTotal = totalCartValue(shoppingCartJSON);
+console.log(realTotal.toFixed(2));
 
 //Task 10
-let gameProgress = [
-    {level: 1},
-    {score: 20},
-    {livesRemaining: 2}
-]
+let gameProgress = {
+    level: 2,
+    score: 20,
+    livesRemaining: 2,
+}
 let gameProgressJSON = JSON.stringify(gameProgress);
 console.log(gameProgressJSON);
 
-let gameProgressObject = JSOM.parse(gameProgress);
-gameProgressUpdated = gameProgressObject[1].value + 100;
-console.log(gameProgressUpdated);
+let gameProgressObject = JSON.parse(gameProgressJSON);
+gameProgressObject.score = gameProgressObject.score + 100;
+console.log(gameProgressObject);
