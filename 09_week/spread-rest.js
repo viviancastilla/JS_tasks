400e86c943efcafa2757392be3d0e037357ed17a
viviancastilla <vivi.castilla.gzz@hotmail.com>
2025-03-17 16:14:36 +0200

    spread-rest git commit -m spread-rest node spread-rest.js

---
+++ 09_week/spread-rest.js
@@ -0,0 +1,44 @@
+'use strict';
+
+//Task 1
+const fruits = ["apple", "banana", "orange"];
+
+const newFruits = [...fruits];
+newFruits.push("grape");
+
+console.log(fruits);
+
+//Task 2
+const oldTask = ["task1", "task2"];
+const newTasks = ["task3", "task4"];
+
+const allTasks = [...oldTask, ...newTasks];
+
+console.log(allTasks);
+
+//Task 3
+const defaultSettings = {theme: "light", fontSize: 14};
+const userSettings = { theme: "dark"};
+
+const{theme, fontSize} = {...defaultSettings, ...userSettings};
+const finalSettings = {theme, fontSize};
+
+console.log(finalSettings);
+
+//Task 4 
+function sumNumbers(...numbers){
+    return numbers.reduce((sum, num) => sum + num, 0);
+}
+console.log(sumNumbers(2, 3, 4));
+
+//Task 5
+const book = {title: "JavaScript Guide", pages: 400, author: "Jhon Doe"};
+
+const {title, ...extraInfo} = book;
+console.log(title, extraInfo);
+
+//Task 6
+function average(){
+    let numbers = [];
+    let sum = (...numbers);
+}
